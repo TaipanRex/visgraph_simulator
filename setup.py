301556dc8e4626caac98a3def5e541db598a217a
Christian Reksten-Monsen <christian@reksten-monsen.com>
2018-06-03 15:46:28 +0800

    push 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ executables = [cx_Freeze.Executable("visgraph_simulator/visgraph_simulator.py")]
 
 cx_Freeze.setup(
     name="Visibility Graph Simulator",
-    version="0.1.0",
+    version="0.1.1",
     options={"build_exe": {"packages":["pygame", "pyvisgraph", "tqdm"]}},
     executables = executables
     )
